5217ef8fa0308d7bd0bd79837b4773e3a892e294
Rasmus Östersjö <viprocoding@gmail.com>
2014-12-15 21:43:04 +0100

    A simple test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "array.h"
 #include <iostream>
 
+#define N 10
+
 int main(void)
 {
+	raos::array<int> a(N);
+
+	for (int i = 0; i < N; i++)
+		std::cout << (a[i] = i) << " ";
+	std::cout << std::endl;
+
 	return 0;
 }
