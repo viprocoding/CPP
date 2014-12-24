c2f48482eda62a538bff188d3ceed383d7ea28cd
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2014-12-24 17:22:21 +0100

    Retab

--- List/List.h
+++ List/List.h
@@ -2,204 +2,204 @@
 #define __LIST_H__
 
 // Libraries
-#include <utility>				// get all comparision operators
+#include <utility>              // get all comparision operators
 
 // My headers
 #include "Node.h"
 
 // namespaces
-using namespace std::rel_ops;	// get all comparison operators
+using namespace std::rel_ops;   // get all comparison operators
 
 template<class T>
 class List {
 private:
 // Life cycle
-	
-	/** Default constructor
-	 */
-	List(void);
+    
+    /** Default constructor
+     */
+    List(void);
 
-	/** Copy constructor
-	 * 
-	 * @param from			This object is copied to this list (deep).
-	 */
-	List(const List<T>& from);
+    /** Copy constructor
+     * 
+     * @param from          This object is copied to this list (deep).
+     */
+    List(const List<T>& from);
 
-	/** Move constructor
-	 *
-	 * @param from			This object is copied to this list (stolen).
-	 */
-	List(const List<T>&& from);
+    /** Move constructor
+     *
+     * @param from          This object is copied to this list (stolen).
+     */
+    List(const List<T>&& from);
 
-	/** Destructor
-	 */
-	~List(void);
+    /** Destructor
+     */
+    ~List(void);
 
 // Operators
 
-	/** Assignment operator
-	 * 
-	 * @param from			This object is assigned to this list (deep).
-	 * @return				This object.
-	 */
-	const List<T>& operator=(const List<T>& from);
+    /** Assignment operator
+     * 
+     * @param from          This object is assigned to this list (deep).
+     * @return              This object.
+     */
+    const List<T>& operator=(const List<T>& from);
 
-	/** Move assignment operator
-	 *
-	 * @param from			This object is assigned to this list (stolen).
-	 * @return				This object.
-	 */
-	const List<T>& operator=(const List<T>&& from);
+    /** Move assignment operator
+     *
+     * @param from          This object is assigned to this list (stolen).
+     * @return              This object.
+     */
+    const List<T>& operator=(const List<T>&& from);
 
-	/** Equal to operator
-	 * 
-	 * @param obj			List to compare with this object.
-	 * @return				true / false.
-	 */
-	bool operator==(const List<T>& obj);
+    /** Equal to operator
+     * 
+     * @param obj           List to compare with this object.
+     * @return              true / false.
+     */
+    bool operator==(const List<T>& obj);
 
-	/** Greater than operator
-	 * 
-	 * @param obj			List to compare with this object.
-	 * @return				true / false.
-	 */
-	bool operator>(const List<T>& obj);
+    /** Greater than operator
+     * 
+     * @param obj           List to compare with this object.
+     * @return              true / false.
+     */
+    bool operator>(const List<T>& obj);
 
 // Operations
 
-	/** Add new node by position
-	 * 
-	 * @param data			Data to store in the new node.
-	 * @param pos			List position to insert the new node.
-	 * @return 				Reference to this object.
-	 *
-	 * @invalid_argument	An exception is generated if position is invalid.
-	 */
-	List<T>& add(const int& pos);
+    /** Add new node by position
+     * 
+     * @param data          Data to store in the new node.
+     * @param pos           List position to insert the new node.
+     * @return              Reference to this object.
+     *
+     * @invalid_argument    An exception is generated if position is invalid.
+     */
+    List<T>& add(const int& pos);
 
-	/** Add new node in head or tail
-	 * 
-	 * @param data			Data to store in the new node.
-	 * @param mode			Control character (valid: 'h' or 't')
-	 * @return				Reference to this object.
-	 *
-	 * @invalid argument	An exception is generated if mode was invalid.
-	 */
-	List<T>& add(const char& mode);
+    /** Add new node in head or tail
+     * 
+     * @param data          Data to store in the new node.
+     * @param mode          Control character (valid: 'h' or 't')
+     * @return              Reference to this object.
+     *
+     * @invalid argument    An exception is generated if mode was invalid.
+     */
+    List<T>& add(const char& mode);
 
-	/** Remove node by position
-	 * 
-	 * @param pos			List position to insert the new node.
-	 * @return 				Data stored in the removed node.
-	 *
-	 * @invalid_argument	An exception is generated if position is invalid.
-	 */
-	T& rm(const int& pos);
+    /** Remove node by position
+     * 
+     * @param pos           List position to insert the new node.
+     * @return              Data stored in the removed node.
+     *
+     * @invalid_argument    An exception is generated if position is invalid.
+     */
+    T& rm(const int& pos);
 
-	/** Remove node in head or tail
-	 * 
-	 * @param mode			Control character (valid: 'h' or 't')
-	 * @return 				Data stored in the removed node.
-	 *
-	 * @invalid_argument	An exception is generated if position is invalid.
-	 */
-	T& rm(const char& pos);
+    /** Remove node in head or tail
+     * 
+     * @param mode          Control character (valid: 'h' or 't')
+     * @return              Data stored in the removed node.
+     *
+     * @invalid_argument    An exception is generated if position is invalid.
+     */
+    T& rm(const char& pos);
 
-	/** Remove all nodes in the list
-	 *
-	 * @return				Reference to this object.
-	 */
-	List<T>& clear(void);
+    /** Remove all nodes in the list
+     *
+     * @return              Reference to this object.
+     */
+    List<T>& clear(void);
 
-	/** Reverse the list
-	 *
-	 * @return				Reference to this object.
-	 */
-	List<T>& reverese(void);
+    /** Reverse the list
+     *
+     * @return              Reference to this object.
+     */
+    List<T>& reverese(void);
 
-	/** Merge lists
-	 *
-	 * @param with			List to merge to this object (steal).
-	 * @param pos			List position to do the merge.
-	 * @return				Reference to this object.
-	 *
-	 * @invalid_argument	An exception is generated if position is invalid.
-	 */
-	List<T>& merge(const int& pos, List<T>& with);
+    /** Merge lists
+     *
+     * @param with          List to merge to this object (steal).
+     * @param pos           List position to do the merge.
+     * @return              Reference to this object.
+     *
+     * @invalid_argument    An exception is generated if position is invalid.
+     */
+    List<T>& merge(const int& pos, List<T>& with);
 
-	/** Merge lists in head or tail
-	 *
-	 * @param with			List to merge to this object (steal).
-	 * @param mode			Mode how to merge (valid: 'h' or 't').
-	 * @return				Reference to this object.
-	 *
-	 * @invalid_argument	An exception is generated if position is invalid.
-	 */
-	List<T>& merge(const char &mode, List<T>& with);
+    /** Merge lists in head or tail
+     *
+     * @param with          List to merge to this object (steal).
+     * @param mode          Mode how to merge (valid: 'h' or 't').
+     * @return              Reference to this object.
+     *
+     * @invalid_argument    An exception is generated if position is invalid.
+     */
+    List<T>& merge(const char &mode, List<T>& with);
 
 
 // Access
 
-	/** Get size
-	 *
-	 * @return				Current size of the list.
-	 */
-	const int& size(void) const;
+    /** Get size
+     *
+     * @return              Current size of the list.
+     */
+    const int& size(void) const;
 
-	/** Is list empty?
-	 *
-	 * @return				true or false.
-	 */
-	bool isEmpty(void) const;
+    /** Is list empty?
+     *
+     * @return              true or false.
+     */
+    bool isEmpty(void) const;
 
-	/** Search node
-	 * 
-	 * @param key			Node to be searched for.
-	 * @return				Position in the list, less than 0 if no match.
-	 *
-	 * -- Maybe generate exception such as no_match, and switch all int to
-	 *    unsigned. --
-	 */
-	int search(const T& key) const;
+    /** Search node
+     * 
+     * @param key           Node to be searched for.
+     * @return              Position in the list, less than 0 if no match.
+     *
+     * -- Maybe generate exception such as no_match, and switch all int to
+     *    unsigned. --
+     */
+    int search(const T& key) const;
 
-	/** Peek at position
-	 *
-	 * @param pos			Possition to peek at.
-	 * @return				Data stored in the specified position.
-	 *
-	 * @invalid_argument	An exception is generated.
-	 */
-	const T& peek(const int& pos) const;	
-	
-	/** Peek head or tail.
-	 *
-	 * @param mode			Control character (valid: 'h' or 't').
-	 * @return				Data stored in the specified position.
-	 *
-	 * @invalid_argument	An exception is generated.
-	 */
-	const T& peek(const char& mode) const;	
+    /** Peek at position
+     *
+     * @param pos           Possition to peek at.
+     * @return              Data stored in the specified position.
+     *
+     * @invalid_argument    An exception is generated.
+     */
+    const T& peek(const int& pos) const;    
+    
+    /** Peek head or tail.
+     *
+     * @param mode          Control character (valid: 'h' or 't').
+     * @return              Data stored in the specified position.
+     *
+     * @invalid_argument    An exception is generated.
+     */
+    const T& peek(const char& mode) const;  
 
-	/** Prints the list
-	 *
-	 * @param start			Specifies position to start printing.
-	 * @param end			Specifies position (inclusive) to stop printing.
-	 * @return				Reference to this object.
-	 *
-	 * @invalid_argument	An exception is generated if start < end or if
-	 *						start, end out of position.
-	 */
-	List<T>& print(void) const;
+    /** Prints the list
+     *
+     * @param start         Specifies position to start printing.
+     * @param end           Specifies position (inclusive) to stop printing.
+     * @return              Reference to this object.
+     *
+     * @invalid_argument    An exception is generated if start < end or if
+     *                      start, end out of position.
+     */
+    List<T>& print(void) const;
 
-	/** Sort the list (merge sort)
-	 *
-	 * @return 				Reference to this object.
-	 */
-	List<T>& sort(void) const;
+    /** Sort the list (merge sort)
+     *
+     * @return              Reference to this object.
+     */
+    List<T>& sort(void) const;
 
 protected:
-	Node<T>* head;		// List head
-	int n;				// List size
+    Node<T>* head;      // List head
+    int n;              // List size
 
 };
 
--- List/Node.h
+++ List/Node.h
@@ -6,151 +6,151 @@ class Node {
 public:
 // Life cycle
 
-	/** Default constructor
-	 */
-	Node(void);
+    /** Default constructor
+     */
+    Node(void);
 
-	/** Constructor
-	 *
-	 * @param Data		Data to initialize the node with.
-	 * @param Next		Pointer to next node, NULL if not specified.
-	 */
-	Node(const T& Data, Node<T>* Next = nullptr);
+    /** Constructor
+     *
+     * @param Data      Data to initialize the node with.
+     * @param Next      Pointer to next node, NULL if not specified.
+     */
+    Node(const T& Data, Node<T>* Next = nullptr);
 
-	/** Copy constructor
-	 *
-	 * @param from		Node to copy to this node with. Here, from.data is
-	 *					written to this->data, and this->next is set to nullptr.
-	 */
-	Node(const Node<T>& from);
+    /** Copy constructor
+     *
+     * @param from      Node to copy to this node with. Here, from.data is
+     *                  written to this->data, and this->next is set to nullptr.
+     */
+    Node(const Node<T>& from);
 
-	/** Copy constructor (move version)
-	 *
-	 * There's no use for a move constructor here. Uncomment this if it's needed
-	 * in the future.
-	 */
-	//Node(const Node<T>&& from);
+    /** Copy constructor (move version)
+     *
+     * There's no use for a move constructor here. Uncomment this if it's needed
+     * in the future.
+     */
+    //Node(const Node<T>&& from);
 
-	/** Destructor
-	 * 
-	 * A destructor is not necessary here.  Uncomment this if it's nedded in the
-	 * future.
-	 */
-	//~Node(void);
+    /** Destructor
+     * 
+     * A destructor is not necessary here.  Uncomment this if it's nedded in the
+     * future.
+     */
+    //~Node(void);
 
 // Operators
-	
-	/** Assignment operator
-	 *
-	 * @param from		Node to assign to this node. Here, from.data is written
-	 *					to this->data, and next is left unchanged.
-	 */
-	const Node<T>& operator=(const Node<T>& from);
+    
+    /** Assignment operator
+     *
+     * @param from      Node to assign to this node. Here, from.data is written
+     *                  to this->data, and next is left unchanged.
+     */
+    const Node<T>& operator=(const Node<T>& from);
 
-	/** Assignment operator (move version)
-	 *
-	 * A move version of the assignment operator is not necessary here.
-	 * Uncomment this if it's needed in the future.
-	 */
-	//const Node<T>& operator=(const Node<T>&& from);
+    /** Assignment operator (move version)
+     *
+     * A move version of the assignment operator is not necessary here.
+     * Uncomment this if it's needed in the future.
+     */
+    //const Node<T>& operator=(const Node<T>&& from);
 
 // Operations
-	
-	/** Set data
-	 * 
-	 * @param Data		Value to write to this->data.
-	 * @return			Reference to this node.
-	 */
-	Node<T>& setData(const T& Data);
+    
+    /** Set data
+     * 
+     * @param Data      Value to write to this->data.
+     * @return          Reference to this node.
+     */
+    Node<T>& setData(const T& Data);
 
-	/** Set next node
-	 *
-	 * @param Next		Pointer to a node that is to be assigned to this->next.
-	 * @return			Reference to this node.
-	 */
-	Node<T>& setNext(const Node<T>& Next);
+    /** Set next node
+     *
+     * @param Next      Pointer to a node that is to be assigned to this->next.
+     * @return          Reference to this node.
+     */
+    Node<T>& setNext(const Node<T>& Next);
 
 // Access
 
-	/** Get data
-	 *
-	 * @return			The data stored in this->data.
-	 */
-	const T& getData(void) const;
+    /** Get data
+     *
+     * @return          The data stored in this->data.
+     */
+    const T& getData(void) const;
 
-	/** Get next node
-	 * 
-	 * @return			Pointer to the next node.	
-	 */
-	Node<T>* getNext(void) const;
+    /** Get next node
+     * 
+     * @return          Pointer to the next node.   
+     */
+    Node<T>* getNext(void) const;
 
-	/** Get this node
-	 *
-	 * @return			Pointer to this node.
-	 */
-	Node<T>* getThis(void) const;
+    /** Get this node
+     *
+     * @return          Pointer to this node.
+     */
+    Node<T>* getThis(void) const;
 
 
 protected:
-	T data;			// Node data
-	Node<T>* next;	// Ptr to next node
+    T data;         // Node data
+    Node<T>* next;  // Ptr to next node
 
 };
 
 template<class T>
 Node<T>::Node(void)
-	: next(nullptr)
+    : next(nullptr)
 {
 }
 
 template<class T>
 Node<T>::Node(const T& Data, Node<T>* Next)
-	: data(Data), next(Next)
+    : data(Data), next(Next)
 {
 }
 
 template<class T>
 Node<T>::Node(const Node<T>& from)
-	: data(from.data), next(nullptr)
+    : data(from.data), next(nullptr)
 {
 }
 
 template<class T>
 const Node<T>& Node<T>::operator=(const Node<T>& from)
 {
-	data = from.data;
-	return *this;
+    data = from.data;
+    return *this;
 }
 
 template<class T>
 Node<T>& Node<T>::setData(const T& Data)
 {
-	data = Data;
-	return *this;
+    data = Data;
+    return *this;
 }
 
 template<class T>
 Node<T>& Node<T>::setNext(const Node<T>& Next)
 {
-	next = &Next;
+    next = &Next;
 }
 
 template<class T>
 const T& Node<T>::getData(void) const
 {
-	return data;
+    return data;
 }
 
 template<class T>
 Node<T>* Node<T>::getNext(void) const
 {
-	return next;
+    return next;
 }
 
 template<class T>
 Node<T>* Node<T>::getThis(void) const
 {
-	return this;
+    return this;
 }
 
 #endif // __NODE_H__
