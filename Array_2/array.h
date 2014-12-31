b53a339ea55d46cedf70ff380bf58a4642e29958
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2014-12-31 13:46:33 +0100

    Started new project

---
+++ Array_2/array.h
@@ -0,0 +1,19 @@
+#ifndef ARRAY_2_H
+#define ARRAY_2_H
+
+template<class T, size_t N>
+class array {
+public:
+// Life cycle
+
+// Operators
+
+// Operations
+
+// Access
+
+// Iterators
+private:
+};
+
+#endif // ARRAY_2_H
